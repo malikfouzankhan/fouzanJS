277c6d042d3494a86ca1fe4a53aba5429958c473
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-20 14:33:28 +0000

    updated 2.js

--- 2.js
+++ 2.js
@@ -12,5 +12,26 @@ Variables basically store ==> number/text and other data types.
 */
 
 // How to declare a variable
+// Semicolons are optional in JS
 
-var box = 1;
+var shortName = "MFK";
+var fullName = 'Malik Fouzan Khan'
+var location = `Hyderabad`
+console.log(shortName)
+console.log(fullName)
+console.log(location)
+
+var college = "Matrusri Engineering College"
+console.log(college)
+
+var age = 21
+console.log(age)
+
+var phone = `+91 123456789`
+console.log(phone)
+
+var address = `5th avenue, US`
+console.log(address)
+
+var favFood = `Biryani, Tahari, Mandi`
+console.log(favFood)
