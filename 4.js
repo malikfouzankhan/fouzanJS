b2e5e33aa438d7937a527f5e299d10acef78f258
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-20 15:16:58 +0000

    added 4.js

---
+++ 4.js
@@ -0,0 +1,6 @@
+let fullName = "Malik Fouzan Khan"
+let age = 21
+let phone = `+91 123456789`
+let laptopName = "Lenovo LOQ"
+
+console.log(`My name is ${fullName}. My age is ${age} and my conatact number is ${phone}. I use ${laptopName} laptop.`)
