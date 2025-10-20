3bbd66a30cf4d26295c4371bff817c9f47371b50
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-20 14:18:31 +0000

    added 1.js and 2.js

---
+++ 1.js
@@ -0,0 +1,15 @@
+// To print anything 
+console.log("Hello World")
+console.log("I am Malik Fouzan")
+console.log("I am an engineering graduate")
+console.log("from Matrusri Engineering College.")
+console.log("I am a part of cohort B-25 at CFI.")
+// This is a single line comment
+/*
+This
+is
+a
+multi
+line
+comment
+*/
---
+++ 2.js
@@ -0,0 +1,16 @@
+// Variables and constants
+
+/*
+There are 3 types of variables in JS.
+1. var
+2. let
+3. const
+
+Variables are containers (storage areas) for data/text/value.
+Variables basically store ==> number/text and other data types.
+
+*/
+
+// How to declare a variable
+
+var box = 1;
