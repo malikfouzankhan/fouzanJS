0d16e66ce8debc4c07b3d0901cbdacfd98e0c3b7
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-21 14:16:30 +0000

    added 7.js

--- 7.js
+++ 7.js
@@ -1,21 +1,19 @@
 // Global scope
 
-
-var i = 1
-let j = 2
-const k = 3
+var i = 1;
+let j = 2;
+const k = 3;
 
 {
-    console.log(i);
-    console.log(j);
-    console.log(k);
+  console.log(i);
+  console.log(j);
+  console.log(k);
 }
 
-print()
+print();
 
-function print()
-{
-    console.log(i);
-    console.log(j);
-    console.log(k);
+function print() {
+  console.log(i);
+  console.log(j);
+  console.log(k);
 }
