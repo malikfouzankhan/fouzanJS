bbe35a40a459c8cb86a38cba17ddac5804424186
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-21 14:55:44 +0000

    added 9.js

---
+++ 9.js
@@ -0,0 +1,39 @@
+// Operators in JavaScript
+
+/*
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Bitwise Operators
+6. Ternary Operators
+7. Typeof Operators
+8. String Operators
+9. Nulling Coalescing & Optional Chaining
+10. Spread & Rest Operators
+*/
+
+/*-------------             --------------*/
+
+// 1. Arithmetic Operators
+/*
+
+| Operator | Desc     | Example | Result |
+| -------- | -------- | ------- | ------ |
+|    +     | Add      |  5 + 2  |   7    |
+|    -     | Sub      |  5 - 2  |   3    |
+|    *     | Multiply |  5 * 2  |   10   |
+|    /     | Divide   |  5 / 2  |   2.5  |
+|    %     | Modulus  |  5 % 2  |   1    |
+|    **    | Exponent |  5 ** 2 |   25   |
+
+*/
+
+let a = 5
+let b = 2
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
