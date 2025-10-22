1ff9926bd5219ec62dad549235229d2d8b29c431
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-22 14:04:00 +0000

    added 13.js

---
+++ 13.js
@@ -0,0 +1,42 @@
+// Comparison operators
+
+let a = 10
+let b = 2
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a === b,"\n");
+
+let c = "10"
+let d = "02"
+console.log(c > d);
+console.log(c < d);
+console.log(c >= d);
+console.log(c <= d);
+console.log(c == d);
+console.log(c === d,"\n");
+
+let e = true
+let f = false
+console.log(e > f);
+console.log(e < f);
+console.log(e >= f);
+console.log(e <= f);
+console.log(e == f);
+console.log(e === f);
+
+
+let num = 10
+let char = "10"
+console.log(num == char);
+console.log(num === char);
+console.log(num > char);
+console.log(num < char);
+console.log(num >= char);
+console.log(num <= char);
+console.log(num >= char);
+console.log(num <= char);
+console.log(num != char);
+console.log(num !== char);
