361a7a176d99492400bf8c19eb96b480605748b2
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-22 12:13:42 +0000

    added 10.js

---
+++ 10.js
@@ -0,0 +1,29 @@
+// Assignment Operator
+
+/*
+
+| Operator | Example  | Same as     |
+| -------- | -------  | -------     |
+|    =     |  x = 5   |  x = 5      | 
+|    +=    |  x += 5  |  x = x + 5  |
+|    -=    |  x -= 5  |  x = x - 5  |
+|    *=    |  x *= 5  |  x = x * 5  |
+|    /=    |  x /= 5  |  x = x / 5  |
+|    %=    |  x %= 5  |  x = x % 5  |
+|    **=   |  x **= 5 |  x = x ** 5 |
+
+*/
+
+let box = 10
+box += 5
+console.log(box);
+box -= 5
+console.log(box);
+box *= 5
+console.log(box);
+box /= 5
+console.log(box);
+box %= 5
+console.log(box);
+box **= 5
+console.log(box);
