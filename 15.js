dd92b8a6529089517d6e886e982fef2203687053
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-22 14:56:45 +0000

    added 15.js

---
+++ 15.js
@@ -0,0 +1,33 @@
+// Typeof Operator
+
+let fName = "MFK"
+let age = 21
+let isAlive = true
+let balance = 998877665544n
+let sleeping = null
+let laptop
+console.log(laptop, "\n");
+
+console.log(typeof fName);
+console.log(typeof age);
+console.log(typeof isAlive);
+console.log(typeof balance);
+console.log(typeof sleeping);
+console.log(typeof laptop, "\n\n\n");
+
+
+let mydets = {
+    naam : "Malik Fouzan Khan",
+    kaam : "CODER",
+    paisa : 100,
+    kharza : 123456123456123n,
+    jayedad : null
+}
+
+console.log(typeof mydets);
+
+let arey = ["mai", 1, 999999999999999999999n, null]
+console.log(arey);
+console.log(typeof arey);
+
+// By default, all non-primitive datatypes are objects.
