3bcf13dc08e8ffc05d156657070493624ece5d38
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-23 13:49:06 +0000

    added 17.js

---
+++ 17.js
@@ -0,0 +1,19 @@
+// All truth values
+/*
+- True boolean : true
+- Non-empty strings : "hello", "false", " "
+- Numbers (except 0) : 1, 10, 999 
+- Arrays & Objects : [], {}, [1,2]
+*/
+
+
+
+// All false values
+/*
+- False boolean : false
+- Empty sring : "". ''
+- Number 0 : 0, -0
+- Null value : null
+- Undefined : undefined
+- Not a Number : NaN
+*/
