62c7aaebda487eb2ba3fb07d55afd1492abf7b64
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-24 14:20:34 +0000

    updated 18.js & 19.js and added 21.js

--- 21.js
+++ 21.js
@@ -1,20 +1,16 @@
 // Control Statements (Part 3/3)
 // Break and Continue Statements
 
-for(let i = 1;i <= 5;i++)
-{
-    console.log(i);
-    if(i == 3)
-    {
-        break;
-    }
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+  if (i == 3) {
+    break;
+  }
 }
 
-for(let j = 1;j <= 5;j++)
-{
-    if(j == 3)
-    {
-        continue;
-    }
-    console.log(j);
+for (let j = 1; j <= 5; j++) {
+  if (j == 3) {
+    continue;
+  }
+  console.log(j);
 }
