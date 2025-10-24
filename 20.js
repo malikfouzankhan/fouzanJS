ed06f1f8ebff336f6b1de2c402a0c38bbf3cb214
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-24 14:17:08 +0000

    added 20.js

---
+++ 20.js
@@ -0,0 +1,19 @@
+// Loop practice
+let i = 1;
+for (i; i <= 10; i++) {
+  console.log(`5 x ${i} = ${5 * i}`);
+}
+
+
+i = 1;
+while (i <= 10) {
+  console.log(`10 x ${i} = ${10 * i}`);
+  i++;
+}
+
+
+i = 1;
+do {
+  console.log(`15 x ${i} = ${15 * i}`);
+  i++;
+} while (i <= 10);
