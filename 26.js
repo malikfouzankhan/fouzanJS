8c44f07fdbe1655236a7817fe72208643471cfa0
malikfouzankhan <malikfouzan05@gmail.com>
2025-10-28 13:29:00 +0000

    added 26.js

--- 26.js
+++ 26.js
@@ -1,7 +1,44 @@
 // Objects in JS
 // It is a non-primitive datatype
 // Stores data in key-value pairs
+// 
 
 let person = {
-    
-}
+    fName : "Fouzan",
+    age : 21,
+    isAlive : true,
+    money : null,
+    peace : undefined
+};
+// console.log(person.fName);
+// console.log(person.age);
+// console.log(person.isAlive);
+// console.log(person.money);
+// console.log(person.peace);
+
+// console.log(person["fName"]);
+// console.log(person["age"]);
+// console.log(person["isAlive"]);
+// console.log(person["money"]);
+// console.log(person["peace"]);
+
+// CRUD
+// C -> Create
+// R -> Read
+// U -> Update
+// D -> Delete
+
+// Access - Read
+console.log(person.fName);
+
+// Create - Create
+person.address = "Hyd";
+console.log(person);
+
+// Update - Update
+person.address = "Tolichowki";
+console.log(person);
+
+// Delete - Delete
+delete person.address
+console.log(person);
