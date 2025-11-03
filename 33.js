0932bea93978a0d43a60b876c514ffe7c5dd2a7d
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-03 09:31:17 +0000

    added 33.js

--- 33.js
+++ 33.js
@@ -10,3 +10,39 @@
     // common use case : creating new arrays
     // creates a new array with modified values and returns in a new array by default
 
+// 22. filter() method
+    let arr2 = [10,20,30,40,50];
+    console.log(arr2.filter((x) => x > 30));
+    let students = [
+        {
+            fullName : "Fouzan",
+            age : 21,
+            college : "MECS"
+        },
+        {
+            fullName : "Rupjith",
+            age : 20,
+            college : "Drop"
+        },
+        {
+            fullName : "Abdullah",
+            age : 22,
+            college : "Drop"
+        }
+    ]
+    console.log(students.filter((x) => x.age > 21));
+    // creates a new array filled with elements that pass a test(a condition you define in a function)
+    // It does not change the original array
+
+// 23. slice() method
+    let arr3 = [1,2,3,4,5,6,7,8,9,10];
+    console.log(arr3.slice(5,9));
+    // used to copy or extract a portion of an array(or string) wihtout changing the original array
+
+// 24. some() method
+    let arr4 = [1,2,3,4,5,6,7,8,9];
+    console.log(arr4.every((x) => x > 7));
+    console.log(arr4.some((x) => x > 7));
+    // checks if at least one of the elements in the array passes a test
+    // returns true if any one element passes the test and returns false if all fails to pass the test
+
