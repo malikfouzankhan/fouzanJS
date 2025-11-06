0fcede6a0e7cccb9a44b7e27a0dc89ff75ee6fc2
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-06 13:34:07 +0000

    added 47.js 48.js

---
+++ 47.js
@@ -0,0 +1,11 @@
+// import and export in JS
+// this is EXPORT file
+function add(a,b){
+    return a + b;
+}
+
+function sub(a,b){
+    return a - b;
+}
+export {add, sub};
+// console.log("This is outside function"); // this is also exported and executed
---
+++ 48.js
@@ -0,0 +1,5 @@
+// import and export in JS
+// this is IMPORT file
+import {add, sub} from "./47.js"
+console.log(add(10,15));
+console.log(sub(15,10));
